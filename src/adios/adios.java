c55b0b4280557111e972cc595fa4602f41aa49db
profevanesa <vanesafpinformatica@gmail.com>
2025-10-19 14:34:59 +0200

    he modificado archivo adios.java

--- src/adios/adios.java
+++ src/adios/adios.java
@@ -2,6 +2,6 @@ package adios;
 
 public class adios {
         public static void main(String[] args) throws Exception {
-        System.out.println("Adios, Vanesa!");
+        System.out.println("Adios, Vanesa! ¿que tal?");
     }
 }
