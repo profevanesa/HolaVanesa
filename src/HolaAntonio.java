775ea5a9c00d9e1c2fdb2ca720203fbaa47c39ab
profevanesa <vanesafpinformatica@gmail.com>
2025-10-19 14:19:06 +0200

    subo todo el proyecto

---
+++ src/HolaAntonio.java
@@ -0,0 +1,5 @@
+public class HolaAntonio {
+      public static void main(String[] args) throws Exception {
+        System.out.println("Hola, Antonio!");
+    }  
+}
---
+++ src/adios/adios.java
@@ -0,0 +1,7 @@
+package adios;
+
+public class adios {
+        public static void main(String[] args) throws Exception {
+        System.out.println("Adios, Vanesa!");
+    }
+}
